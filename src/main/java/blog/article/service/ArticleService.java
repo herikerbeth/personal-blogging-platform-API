398b9eeb95f2304e08b198c7fd5d922aa6e81b4f
herikerbeth <herik.docarmo@hotmail.com>
2024-08-07 18:22:46 -0300

    Create ArticleService interface.

---
+++ src/main/java/blog/article/service/ArticleService.java
@@ -0,0 +1,17 @@
+package blog.article.service;
+
+import blog.article.model.Article;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public interface ArticleService {
+
+    List<Article> findAll();
+    Article findById(Long id);
+    Article createArticle(Article article);
+    void deleteArticle(Long id);
+    Article updateArticle(Article article);
+
+}
