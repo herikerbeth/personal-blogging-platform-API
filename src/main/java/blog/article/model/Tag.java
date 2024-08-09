38ad16d19f5715993252e06b4c945a07ddaabae4
herikerbeth <herik.docarmo@hotmail.com>
2024-08-09 12:04:06 -0300

    Add table name.

--- src/main/java/blog/article/model/Tag.java
+++ src/main/java/blog/article/model/Tag.java
@@ -1,19 +1,16 @@
 package blog.article.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import jakarta.persistence.*;
+import lombok.*;
 
-@Entity
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
+
+@Entity(name = "Tags")
+@Table(name = "Tags")
 public class Tag {
 
     @Id
