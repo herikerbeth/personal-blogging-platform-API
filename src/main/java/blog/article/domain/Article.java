6f985d1ee6e82bc2e42fe9cf3f132b2563bf9bea
herikerbeth <herik.docarmo@hotmail.com>
2024-08-13 10:25:44 -0300

    refactor: Transform Article class into DTO

--- src/main/java/blog/article/domain/Article.java
+++ src/main/java/blog/article/domain/Article.java
@@ -1,29 +1,19 @@
 package blog.article.domain;
 
-import jakarta.persistence.*;
 import lombok.*;
 
 import java.time.LocalDate;
 import java.util.List;
 
-@Getter
-@Setter
+@Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
-
-@Entity(name = "articles")
-@Table(name = "articles")
 public class Article {
-
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+ 
     private Long id;
     private String title;
     private String content;
-
-    @OneToMany
     private List<Tag> tags;
-
-    @Column(name = "publish_date")
     private LocalDate publishDate;
 }
