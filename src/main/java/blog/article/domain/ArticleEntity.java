a2361b729a1a813f748d0f9d563cc21f4bc85178
herikerbeth <herik.docarmo@hotmail.com>
2024-08-13 10:27:38 -0300

    refactor: Create ArticleEntity class to represent JPA entity

---
+++ src/main/java/blog/article/domain/ArticleEntity.java
@@ -0,0 +1,29 @@
+package blog.article.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+
+@Entity(name = "articles")
+@Table(name = "articles")
+public class ArticleEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String content;
+
+    @OneToMany
+    private List<Tag> tags;
+
+    @Column(name = "publish_date")
+    private LocalDate publishDate;
+}
