a9257a86bc733a201ad80648e9f56982f6a060a3
herikerbeth <herik.docarmo@hotmail.com>
2024-08-15 22:42:41 -0300

    refactor: Update method signatures and removed Optional return type

--- src/main/java/blog/article/services/ArticleService.java
+++ src/main/java/blog/article/services/ArticleService.java
@@ -3,14 +3,12 @@ package blog.article.services;
 import blog.article.domain.Article;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface ArticleService {
 
-    Article saveArticle(Article article);
+    Article saveArticle(Article articleDTO);
     List<Article> getAllArticles();
-    Optional<Article> getArticleById(Long id);
-    Article updateArticle(Article article);
+    Article getArticleById(Long id);
+    Article updateArticle(Long id, Article article);
     void deleteArticle(Long id);
-
 }
