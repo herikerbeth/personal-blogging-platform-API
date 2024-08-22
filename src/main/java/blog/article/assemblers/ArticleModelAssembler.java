b9a7aefac377098cc3f5303f0bea755a1396644b
herikerbeth <herik.docarmo@hotmail.com>
2024-08-22 17:04:22 -0300

    feat: update ArticleModelAssembler to use ArticleResponse

--- src/main/java/blog/article/assemblers/ArticleModelAssembler.java
+++ src/main/java/blog/article/assemblers/ArticleModelAssembler.java
@@ -1,7 +1,7 @@
 package blog.article.assemblers;
 
 import blog.article.controllers.ArticleController;
-import blog.article.domain.Article;
+import blog.article.domain.ArticleResponse;
 import org.springframework.hateoas.EntityModel;
 import org.springframework.hateoas.server.RepresentationModelAssembler;
 import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
@@ -11,13 +11,13 @@ import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
 
 
 @Component
-public class ArticleModelAssembler implements RepresentationModelAssembler<Article, EntityModel<Article>> {
+public class ArticleModelAssembler implements RepresentationModelAssembler<ArticleResponse, EntityModel<ArticleResponse>> {
 
     @Override
-    public EntityModel<Article> toModel(Article article) {
+    public EntityModel<ArticleResponse> toModel(ArticleResponse article) {
 
         return EntityModel.of(article,
-                WebMvcLinkBuilder.linkTo(methodOn(ArticleController.class).getArticle(article.getId())).withSelfRel(),
+                WebMvcLinkBuilder.linkTo(methodOn(ArticleController.class).getArticle(article.id())).withSelfRel(),
                 linkTo(methodOn(ArticleController.class).getAllArticles()).withRel("articles"));
     }
 }
