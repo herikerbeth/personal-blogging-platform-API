654b52f59b763692dfc6a4d1aee83351d7a5bf5b
herikerbeth <herik.docarmo@hotmail.com>
2024-08-22 16:24:53 -0300

    feat: add validation constraints to ArticleEntity fields

--- src/main/java/blog/article/domain/ArticleEntity.java
+++ src/main/java/blog/article/domain/ArticleEntity.java
@@ -1,6 +1,7 @@
 package blog.article.domain;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
 import lombok.*;
 
 import java.time.LocalDate;
@@ -18,7 +19,11 @@ public class ArticleEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @NotBlank(message = "Title name cannot be empty")
     private String title;
+
+    @NotBlank(message = "Content cannot be empty")
     private String content;
 
     @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
