fc12e2276bf04855b9e36c72a2aaabc5d21c3622
herikerbeth <herik.docarmo@hotmail.com>
2024-08-25 09:22:05 -0300

    feat: add Swagger annotations to ArticleCreateRequest

--- src/main/java/blog/article/domain/ArticleCreateRequest.java
+++ src/main/java/blog/article/domain/ArticleCreateRequest.java
@@ -1,9 +1,13 @@
 package blog.article.domain;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 import java.util.List;
 
 public record ArticleCreateRequest(
+        @Schema(description = "Title of the article", example = "John")
         String title,
+        @Schema(description = "Content of the article", example = "content")
         String content,
         List<Tag> tags
 ) {}
