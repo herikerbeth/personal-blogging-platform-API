4bf3f7c479489d8bda716052d290de206622d725
herikerbeth <herik.docarmo@hotmail.com>
2024-08-25 09:08:35 -0300

    feat: update OpenAPI server URLs

--- src/main/java/blog/Application.java
+++ src/main/java/blog/Application.java
@@ -5,7 +5,10 @@ import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@OpenAPIDefinition(servers = { @Server(url = "/", description = "Default Server URL")})
+@OpenAPIDefinition(servers = {
+		@Server(url = "https://personal-blogging-platform-api.up.railway.app/", description = "Personal Blogging Platform API"),
+		@Server(url = "http://localhost:8080/", description = "The API is not online. It must run on your local server")
+})
 @SpringBootApplication
 public class Application {
 
