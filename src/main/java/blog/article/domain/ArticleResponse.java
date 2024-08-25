d26a03ba89ced766d7e0167353902a0c448900bb
herikerbeth <herik.docarmo@hotmail.com>
2024-08-25 09:22:39 -0300

    feat: add Swagger annotations to ArticleResponse

--- src/main/java/blog/article/domain/ArticleResponse.java
+++ src/main/java/blog/article/domain/ArticleResponse.java
@@ -1,13 +1,20 @@
 package blog.article.domain;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 import java.time.LocalDate;
 import java.util.List;
 
 public record ArticleResponse(
+        @Schema(description = "ID of the article", example = "1")
         Long id,
+        @Schema(description = "Title of the article", example = "John")
         String title,
+        @Schema(description = "Content of the article", example = "content")
         String content,
+        @Schema(description = "Tags of the article")
         List<Tag> tags,
+        @Schema(description = "Publish date of the article", example = "2024-08-24")
         LocalDate publishDate
 ) {
 
