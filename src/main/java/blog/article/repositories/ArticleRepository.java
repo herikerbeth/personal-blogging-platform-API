84305c547f0bc511205b64278c3450a9b1e007a9
herikerbeth <herik.docarmo@hotmail.com>
2024-08-27 16:53:15 -0300

    feat: add method to find articles by publish date in ArticleRepository

--- src/main/java/blog/article/repositories/ArticleRepository.java
+++ src/main/java/blog/article/repositories/ArticleRepository.java
@@ -4,6 +4,11 @@ import blog.article.domain.ArticleEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
+import java.util.List;
+
 @Repository
 public interface ArticleRepository extends JpaRepository<ArticleEntity, Long> {
+
+    List<ArticleEntity> findAllByPublishDate(LocalDate publishDate);
 }
