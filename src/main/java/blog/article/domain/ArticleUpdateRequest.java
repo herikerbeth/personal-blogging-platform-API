f9ecc795590bf4d0db85cbba54819f57c95560c3
herikerbeth <herik.docarmo@hotmail.com>
2024-09-02 15:13:58 -0300

    refactor: update ArticleUpdateRequest to use TagEntity and TagCreateRequest

--- src/main/java/blog/article/domain/ArticleUpdateRequest.java
+++ src/main/java/blog/article/domain/ArticleUpdateRequest.java
@@ -1,9 +1,17 @@
 package blog.article.domain;
 
+import blog.tag.domain.TagCreateRequest;
+import blog.tag.domain.TagEntity;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Schema;
+
 import java.util.List;
 
 public record ArticleUpdateRequest(
+        @Schema(description = "Title of the article", example = "John")
         String title,
+        @Schema(description = "Content of the article", example = "content")
         String content,
-        List<Tag> tags
+        @ArraySchema(schema = @Schema(description = "Tags of the article", implementation = TagCreateRequest.class))
+        List<TagEntity> tags
 ) {}
