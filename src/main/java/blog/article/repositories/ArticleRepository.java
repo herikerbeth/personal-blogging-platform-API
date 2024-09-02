07a627d341ab660eaff52a716e0d16384179e91e
herikerbeth <herik.docarmo@hotmail.com>
2024-09-02 15:18:44 -0300

    feat: add method to find articles by tag name

--- src/main/java/blog/article/repositories/ArticleRepository.java
+++ src/main/java/blog/article/repositories/ArticleRepository.java
@@ -2,6 +2,8 @@ package blog.article.repositories;
 
 import blog.article.domain.ArticleEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
@@ -11,4 +13,7 @@ import java.util.List;
 public interface ArticleRepository extends JpaRepository<ArticleEntity, Long> {
 
     List<ArticleEntity> findAllByPublishDate(LocalDate publishDate);
+
+    @Query("SELECT a FROM articles a JOIN a.tags t WHERE t.name = :tagName")
+    List<ArticleEntity> findAllByTagsName(@Param("tagName")String tagName);
 }
